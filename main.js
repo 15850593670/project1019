8146f9ac5b5a1c072db468a73d6ab2f2818c5522
David Hu <hux0713@gmail.com>
2016-10-20 01:57:58 +0800

    Configuring global shortcuts working

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ const {app, BrowserWindow, globalShortcut, ipcMain} = require('electron')
 
 let mainWindow = null
 
+var configuration = require('./configuration')
+
 app.on('ready', () => {
+	if (!configuration.readSettings('shortcutKeys')) {
+	    configuration.saveSettings('shortcutKeys', ['ctrl', 'shift']);
+	}
+
 	mainWindow = new BrowserWindow({
 		frame: false,
 		resizable: false,
@@ -15,6 +21,7 @@ app.on('ready', () => {
 		width: 368
 	})
 
+	/*
 	globalShortcut.register('X', () => {
 		mainWindow.webContents.send('global-shortcut', 0);
     })
@@ -22,11 +29,25 @@ app.on('ready', () => {
 	globalShortcut.register('Z', () => {
 	    mainWindow.webContents.send('global-shortcut', 1);
     })
+	*/
 
 	//mainWindow.loadURL('file:///home/david/Desktop/project1019/index.html')
 	mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 })
 
+function setGlobalShortcuts() {
+    globalShortcut.unregisterAll();
+
+    var shortcutKeysSetting = configuration.readSettings('shortcutKeys');
+    var shortcutPrefix = shortcutKeysSetting.length === 0 ? '' : shortcutKeysSetting.join('+') + '+';
+
+    globalShortcut.register(shortcutPrefix + '1', () => {
+		mainWindow.webContents.send('global-shortcut', 0);
+    });
+    globalShortcut.register(shortcutPrefix + '2', () => {
+        mainWindow.webContents.send('global-shortcut', 1);
+    });
+}
 
 ipcMain.on('close-main-window', () => {
 	app.quit();
@@ -57,4 +78,8 @@ ipcMain.on('close-settings-window', () => {
 	if (settingsWindow) {
 	    settingsWindow.close();
     }
+})
+
+ipcMain.on('set-global-shortcuts', () => {
+	    setGlobalShortcuts();
 })
